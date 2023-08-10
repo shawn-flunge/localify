aa749874ce57ee7e41ac5097c631b3b39e3c28db
shawn-flunge <echun1234@gmail.com>
2023-08-10 21:06:00 +0900

    analysis the user input and the setting and make the config object

--- bin/localify.dart
+++ bin/localify.dart
@@ -1,5 +1,22 @@
-import 'package:localify/localify.dart' as localify;
 
-void main(List<String> arguments) {
-  print('Hello world: ${localify.calculate()}!');
+
+import 'package:localify/localify.dart';
+
+void main(List<String> arguments) async{
+  final commandRunner = CommandRunner<Configuration>(programName, 'description')
+    ..addCommand(GenerateCommand());
+
+  try{
+    final result = await commandRunner.run(arguments);
+
+    if(result == null) return;
+
+    // final bool success = await switch (result){
+    //   GenerateConfiguration() => generateOutputFromConfig(result)
+    // // BuildConfiguration() => print(result)
+    // };
+
+  } catch(e){
+    print(e);
+  }
 }
