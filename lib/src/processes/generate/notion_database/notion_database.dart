e0d9ac22adc18fa2be6c4efdf93dfae071043979
shawn-flunge <echun1234@gmail.com>
2023-09-07 21:43:26 +0900

    parsed header from data source

--- lib/src/processes/generate/notion_database/notion_database.dart
+++ lib/src/processes/generate/notion_database/notion_database.dart
@@ -1,7 +1,28 @@
 
 
 
-generateOutputsFromNotionDatabase(){
+import 'dart:convert';
+
+import 'package:localify/localify.dart';
+
+Future<bool> generateOutputsFromNotionDatabase(Configuration config, String rawData) async{
+
+  /// source
+  final Map body = json.decode(rawData);
+  final List results = body['results'];
+
+  /// parse header
+  final header = _parseTableHeader(results[0]['properties'].keys);
+
+  return false;
+}
 
 
+Map<String, Map<String, dynamic>> _parseTableHeader(Iterable<String> languages){
+  final Map<String, Map<String, dynamic>> result = {};
+
+  for(var lang in languages){
+    result.putIfAbsent(lang, () => {});
+  }
+  return result;
 }
