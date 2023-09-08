532e3f40daaf0bac88a8c6c8d866ae2e4c959935
shawn-flunge <echun1234@gmail.com>
2023-09-08 21:32:09 +0900

    parse rows from raw data

--- lib/src/processes/generate/notion_database/notion_database.dart
+++ lib/src/processes/generate/notion_database/notion_database.dart
@@ -4,6 +4,8 @@
 import 'dart:convert';
 
 import 'package:localify/localify.dart';
+import 'package:localify/src/processes/generate/utils/escapes.dart';
+import 'package:localify/src/types.dart' show Rows;
 
 Future<bool> generateOutputsFromNotionDatabase(Configuration config, String rawData) async{
 
@@ -13,6 +15,7 @@ Future<bool> generateOutputsFromNotionDatabase(Configuration config, String rawD
 
   /// parse header
   final header = _parseTableHeader(results[0]['properties'].keys);
+  final rows = _parseRows<(String locale, String value)>(results);
 
   return false;
 }
@@ -25,4 +28,27 @@ Map<String, Map<String, dynamic>> _parseTableHeader(Iterable<String> languages){
     result.putIfAbsent(lang, () => {});
   }
   return result;
+}
+
+
+Rows<T> _parseRows<T>(List body){
+  final escape = Escapes.replace;
+
+  final Rows<T> rows = body.getRange(0, body.length).map((e) {
+
+    final Map properties = e['properties'];
+    final List<T> row = [];
+
+    properties.forEach((key, value) {
+      final type = value['type'];
+      final text = value[type];
+      row.add(
+          (key, text.isEmpty ? '' : escape(text[0]['plain_text'])) as T
+      );
+    });
+
+    return row;
+  }).toList();
+
+  return rows;
 }
