6220bef06f9e12bc27d0e7ffd0bb133a0ae2ca47
shawn-flunge <echun1234@gmail.com>
2023-09-12 21:08:34 +0900

    added _parseToLanguageMap

--- lib/src/processes/generate/notion_database/notion_database.dart
+++ lib/src/processes/generate/notion_database/notion_database.dart
@@ -5,45 +5,36 @@ import 'dart:convert';
 
 import 'package:localify/localify.dart';
 import 'package:localify/src/processes/generate/utils/escapes.dart';
-import 'package:localify/src/types.dart' show Rows;
+import 'package:localify/src/types.dart';
+
+typedef LocalePair = ({String locale, String value});
 
 Future<bool> generateOutputsFromNotionDatabase(Configuration config, String rawData) async{
 
   /// source
   final Map body = json.decode(rawData);
   final List results = body['results'];
+  final rows = _parseRows(results);
 
-  /// parse header
-  final header = _parseTableHeader(results[0]['properties'].keys);
-  final rows = _parseRows<(String locale, String value)>(results);
+  final languages = _parseToLanguageMap(rows);
 
   return false;
 }
 
 
-Map<String, Map<String, dynamic>> _parseTableHeader(Iterable<String> languages){
-  final Map<String, Map<String, dynamic>> result = {};
-
-  for(var lang in languages){
-    result.putIfAbsent(lang, () => {});
-  }
-  return result;
-}
-
-
-Rows<T> _parseRows<T>(List body){
+Rows<LocalePair> _parseRows(List body){
   final escape = Escapes.replace;
 
-  final Rows<T> rows = body.getRange(0, body.length).map((e) {
+  final Rows<LocalePair> rows = body.getRange(0, body.length).map((e) {
 
     final Map properties = e['properties'];
-    final List<T> row = [];
+    final List<LocalePair> row = [];
 
     properties.forEach((key, value) {
       final type = value['type'];
       final text = value[type];
       row.add(
-          (key, text.isEmpty ? '' : escape(text[0]['plain_text'])) as T
+          (locale: key, value: text.isEmpty ? '' : escape(text[0]['plain_text']))
       );
     });
 
@@ -51,4 +42,26 @@ Rows<T> _parseRows<T>(List body){
   }).toList();
 
   return rows;
+}
+
+LanguageMap _parseToLanguageMap(Rows rows){
+
+  final LanguageMap result = {};
+
+  for(var lang in rows[0]){
+    if(lang.locale.toUpperCase() == 'KEY') continue;
+    result.putIfAbsent(lang.locale, () => {});
+    result[lang.locale]!.putIfAbsent('@@locale', () => lang.locale);
+  }
+
+  for (var row in rows) {
+    final keyIndex = row.indexWhere((element) => element.locale.toUpperCase() == 'KEY');
+    final key = row.removeAt(keyIndex);
+
+    for (var cell in row) {
+      result[cell.locale]?.putIfAbsent(key.value, () => cell.value);
+    }
+  }
+
+  return result;
 }
