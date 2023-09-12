c86bc43c25a1997b72dd0fa88fd6319b08da1703
shawn-flunge <echun1234@gmail.com>
2023-09-12 21:09:39 +0900

    added custom type to simplify map

--- lib/src/types.dart
+++ lib/src/types.dart
@@ -1,3 +1,8 @@
 
+/// todo: 설명
+
 /// A custom type that is a result of parsing raw data
 typedef Rows<T> = List<List<T>>;
+
+/// A dart map to generate output files
+typedef LanguageMap = Map<String, Map<String, String>>;
